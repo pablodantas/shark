b607460bcc9335b7814005f1141b161e2ed867f0
Pablo Dantas <85039114+pablodantas@users.noreply.github.com>
2024-02-26 00:29:50 -0300

    Add files via upload

---
+++ assets/js/chartMobile.js
@@ -0,0 +1,35 @@
+var ENERGY_TOTAL = 21000000;
+var energyCostTotal;
+var EnergyCosts;
+var energyKeys = {'Listing Wallet':1,'Prize Portfolio':2,'Pre Sale':3, 'Pancakeswap':4}
+var energyContributions = [
+            ['Listing Wallet 5%', 1050000],
+            ['Prize Portfolio 5%', 1050000],
+            ['Pre Sale 54.5% (70 BNB)', 11410000],
+            ['Pancakeswap 35.4% (49 BNB)', 7416500],
+        ];
+var chart = c3.generate({
+      size: {
+        height: 280,
+        width: 300
+    },
+    bindto: '#energyAllocationMobile',
+    data: {
+        columns: energyContributions,
+        type : 'donut',
+    },
+    color: {
+      pattern: ['#FFFFFF', '#0dcaf0', '#160863','#303bff']
+    },
+    donut: {
+        label: {show: false},
+    },
+    legend: {
+        position: 'bottom',
+        item: {
+          onclick: function () { return false; }
+        }
+    }
+});
+
+/* set a new value for a piece of the energy budget */
